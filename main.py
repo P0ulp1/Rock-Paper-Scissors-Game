18cae2ed1db22fb789145db07c4bbc94009775c1
P0ulp1 <71771197+P0ulp1@users.noreply.github.com>
2022-07-06 14:43:01 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,73 @@
+# Pre Requirements
+import random
+
+# ASCII Art
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+# Real Code
+player_choice = input("What do you choose ? Type '0' for Rock, '1' for Paper or '2' for Scissors.\n")
+computer_choice = random.randint(0, 2)
+
+if player_choice == "0":
+    print(rock)
+elif player_choice == "1":
+    print(paper)
+elif player_choice == "2":
+    print(scissors)
+else:
+    print("Please enter a valid proposition")
+
+print("Computer choose :")
+
+if computer_choice == 0:
+    print(rock)
+elif computer_choice == 1:
+    print(paper)
+elif computer_choice == 2:
+    print(scissors)
+
+if player_choice == "0" and computer_choice == 0:
+    print("Tie.")
+elif player_choice == "0" and computer_choice == 1:
+    print("You loose.")
+elif player_choice == "0" and computer_choice == 2:
+    print("You Win !")
+elif player_choice == "1" and computer_choice == 0:
+    print("You Win !")
+elif player_choice == "1" and computer_choice == 1:
+    print("Tie.")
+elif player_choice == "1" and computer_choice == 2:
+    print("You loose.")
+elif player_choice == "2" and computer_choice == 0:
+    print("You loose.")
+elif player_choice == "2" and computer_choice == 1:
+    print("You Win !")
+elif player_choice == "2" and computer_choice == 2:
+    print("Tie.")
+else:
+    print("This line can't be printed.")
